16c414b16622cc9a1e7097fe32105d3e0715c46d
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2014-12-14 16:48:19 +0300

    initial empty project

---
+++ src/SpicedCloud.Cmd/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SpicedCloud.Cmd
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("hello");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ src/SpicedCloud/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SpicedCloud")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("SpicedCloud")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0b3276b0-be27-4d42-b11d-58d4cda03024")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
